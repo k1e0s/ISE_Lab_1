c5287209fe9227af9954d3c80141d9bd438f2925
Timofey <timofeymukanov000@gmail.com>
2022-11-30 15:37:40 +0300

    Заменил конечное значение цикла в 41 строчке с 6 на 5

--- Program.cs
+++ Program.cs
@@ -38,7 +38,7 @@ namespace Jla6a_TTporpammupoBaHue
 
 
             List<int> numbers = new List<int>();
-            for (int i = 1; i < 6; i++)
+            for (int i = 1; i < 5; i++)
             {
                 numbers.Add(i);
             }
